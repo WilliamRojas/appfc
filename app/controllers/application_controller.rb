5ffb6c05f09e7284aa349fd6fbbb7e947f2cb06f
Your Name <wijeroma@hotmail.com>
2016-05-16 08:22:09 -0500

    sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :password, :remember_me, :role) }
   	devise_parameter_sanitizer.for(:sign_up){|u| u.permit(:username, :email, :password, :password_confirmation, :role)}
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :role, :email, :password, :password_confirmation, :current_password)}
   end
 
 end
